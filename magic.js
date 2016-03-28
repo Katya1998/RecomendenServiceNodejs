d8caffa66e98f280dc0daadf5c64891f11b7a26d
Фильчакова Екатерина Леонидовна <E.L.Filchakova@at.urfu.ru>
2016-03-28 15:33:29 +0500

    magic

--- magic.js
+++ magic.js
@@ -3,12 +3,49 @@ template = require('swig');
 pg = require('pg')
 
 var server = new http.Server(),
-    connectionString = "postgres://postgres:postgres@localhost:5432/ai_recipe";
+    connectionString = "postgres://postgres:postgres@localhost:5432/market";
 
 server.listen(8880, '127.0.0.1');
 
 server.on('request', function(req, res){
+var tmpl = template.compileFile('./templates/new1.html'),
+        film_list = []; 
+var results = []; 
+    pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+       if(err) {
+         done();
+         console.log({ success: false, data: err});
+       }
+
+       var query = client.query("SELECT * FROM films ORDER BY id ASC");//извлекает данные
+
+       query.on('row', function(row) {
+          results.push(row);
+       });
+
+						
+        query.on('end', function() {
+		
+		var row_length = 3;
+	    new_results = [];
+	    for (var i=0; i<3; i++){
+		new_results.push(results[i]);
+		}
+		console.log(new_results)
+		client.end()
+        return res.end(//печатает в браузер
+		tmpl(
+                    {
+                        'film_list': [new_results,
+									new_results],
+                        'title': 'Кино для всех'
+                    }
+                
+            )
+      	)
+       });
+    });
 
 
-console.log("сеpвер работает");
 });
